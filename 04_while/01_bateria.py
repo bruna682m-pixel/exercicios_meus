8d55468e80a07e7ab0f90aa97c1689c3318884f0
bruna <bruna682m@gmail.com>
2026-07-21 12:06:22 -0300

    exercicios de while

---
+++ 04_while/01_bateria.py
@@ -0,0 +1,11 @@
+# %%
+bateria = 0
+
+while bateria <= 100:
+    print("Carregando... A porcetagem atual é de: ", bateria)
+    bateria += 1
+
+print("Bateria carregada!!!!")
+
+
+
---
+++ 04_while/02_vida.py
@@ -0,0 +1,9 @@
+# %%
+vidas = 5
+
+while vidas > 0:
+    vidas -= 1
+    print("voce perdeu 1 vida. Faltam ", vidas, "vidas.")
+    
+print("Suas vidas acabaram.")
+
