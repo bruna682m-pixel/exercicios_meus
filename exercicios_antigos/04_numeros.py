cfa0c644b83700749f3fc5e3a84220d432d37d8b
Abruna682m-pixel <bruna682m@gmail.com>
2026-07-26 16:45:03 -0300

    Atualizar o 04_numeros.py
    pegando 2 números negativos

--- exercicios_antigos/04_numeros.py
+++ exercicios_antigos/04_numeros.py
@@ -1,18 +1,16 @@
 #  Ler dois valores inteiros negativos ( consistir se é negativo), multiplicar e mostrar o resultado
 # %%
 
-count = 0
 multiplicacao = 0
+numero1 = 0
+numero2 = 0
 
-while True:
+while numero1 >= 0 or numero2 >= 0:
     numero1 = input("Digite um numero:")
     numero2 = input("Digite outro numero:")
 
     numero1 = float(numero1)
     numero2 = float(numero2)
-
-    if numero1 <= 0 and numero2 <= 0:
-        break
 
     multiplicacao = numero1 * numero2
 
