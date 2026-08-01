ed63166af61e3c8e10580c37e2d1fcbeba1b3da2
Abruna682m-pixel <bruna682m@gmail.com>
2026-08-01 19:07:04 -0300

    Atualizar o 09_fatorial.py
    resolução completa exercício do fatorial

--- exercicios_antigos/while/09_fatorial.py
+++ exercicios_antigos/while/09_fatorial.py
@@ -71,3 +71,31 @@ while numero <= 1:
         print(copia_total)
 
 print("sai", copia_total)
+
+# %%
+# resultado final
+
+total = 1
+
+numero = input("Digite um número para calcular o fatorial:")
+numero = int(numero)
+
+copia_numero = numero
+
+if numero > 0:
+   while numero >= 1:
+      total = total * numero
+      numero -= 1  
+
+   print("O fatorial de",copia_numero,"é igual a:",total)
+elif numero == 0:
+  print("O fatorial de",copia_numero,"é igual a: 1",)
+else:
+    print("Digite um número positivo") 
+ 
+
+
+  
+
+
+  
