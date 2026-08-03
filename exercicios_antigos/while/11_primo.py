00b532dc3bdaa8b6275917c94635e647e387ef21
Abruna682m-pixel <bruna682m@gmail.com>
2026-08-03 18:25:52 -0300

    Atualizar o 11_primo.py
    concluindo exercício do número primo

--- exercicios_antigos/while/11_primo.py
+++ exercicios_antigos/while/11_primo.py
@@ -49,3 +49,44 @@ while count < numero:
     
 
 print("sai",total)
+
+# %%
+numero = int(input("Digite um número para ver se é primo."))
+count = 2
+primo = True
+
+while numero < 2:
+    numero = int(input("Digite um número para ver se é primo."))
+
+while count < numero:
+    if numero % count == 0:
+       primo = False
+       break
+    count += 1
+    
+if primo:
+      print("Seu número é primo")
+else:
+      print("Seu número não é primo")
+
+
+
+print("sai")
+  
+  
+
+  
+
+
+  
+
+
+  
+  
+
+  
+
+
+  
+
+
