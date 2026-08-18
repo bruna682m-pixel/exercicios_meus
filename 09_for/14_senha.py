2c00c74115ad473cf2c41daea2ca7501a15a139e
Bruna Maria <bruna682m@gmail.com>
2026-08-18 18:59:13 -0300

    Criar 14_senha.py
    exercicio senha com for

---
+++ 09_for/14_senha.py
@@ -0,0 +1,16 @@
+numero_max = 4
+
+primeira_senha = int(input("Defina uma senha:"))
+
+for i in range(1, numero_max+1):
+  tentativa = int(input("Digite a sua 
+                        
+  if primeira_senha == tentativa:
+    print("Você acertou.")
+    break
+ 
+else:
+    print("acabaram as tentativas.")
+    
+  
+ 
