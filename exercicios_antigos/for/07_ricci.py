0cd7d9709104ab833babf5924ac1dd1f3326942e
bruna <bruna682m@gmail.com>
2026-08-28 11:58:45 -0300

    exercícios for primos, Ricci e Fetuccine

--- exercicios_antigos/for/07_ricci.py
+++ exercicios_antigos/for/07_ricci.py
@@ -10,8 +10,10 @@ r1 = int(input("Digite o primeiro número da sequência:"))
 r2 = int(input("Digite o segundo número da sequência:"))
 numero = int(input("posição da serie:"))
 
+print(r1)
+print(r2)
              
-for i in range(1, numero):
+for i in range(1, numero-1):
     soma = r1 + r2
 
     print(soma)
@@ -19,7 +21,36 @@ for i in range(1, numero):
     r1 = r2
     r2 = soma
 
-
-
-
 # %%
+soma = 0
+soma_termos = 0
+soma_2_primeiros = 0
+
+r1 = int(input("Digite o primeiro número da sequência:"))
+r2 = int(input("Digite o segundo número da sequência:"))
+numero = int(input("posição da serie:"))
+
+soma_2_primeiros = r1 + r2
+print(r1)
+print(r2)
+
+if numero > 3:     
+    for i in range(1, numero-1):
+        soma = r1 + r2
+
+        print(soma)
+
+        r1 = r2
+        r2 = soma
+
+        soma_termos = soma_termos + soma
+
+    soma_termos = soma_termos + soma_2_primeiros
+    print("A soma dos",numero,"termos é:", soma_termos)
+
+else:
+    print("A posição da serie deve ser maior que 3.")
+
+
+
+
--- exercicios_antigos/for/08_fetuccine.py
+++ exercicios_antigos/for/08_fetuccine.py
@@ -5,3 +5,74 @@
  #   partir daí , os termos são  gerados com a soma ou subtração dos dois termos anteriores, ou seja:
 #- se o termo for ímpar soma-se os anteriores para se obter o próximo,
 #- se o termo for par subtrai-se os anteriores para se obter o próximo.
+# %%
+
+f1 = int(input("Digite o primeiro termo:"))
+f2 = int(input("Digite o segundo termo:"))
+numero = int(input("Digite a posição do termo:"))
+soma = 0
+subtracao = 0
+
+print(f1)
+print(f2)
+
+if numero > 3:
+    for i in range(1, numero-1):
+        if i % 2 == 0 :
+            subtracao = f1 - f2
+
+            print(subtracao)
+
+            f1 = f2
+            f2 = subtracao
+
+        else:
+            soma = f1 + f2
+
+            print(soma)
+
+            f1 = f2
+            f2 = soma
+else:
+    print("A poisção deve ser maior que 3.")
+
+# %%
+
+f1 = int(input("Digite o primeiro termo:"))
+f2 = int(input("Digite o segundo termo:"))
+numero = int(input("Digite a posição do termo:"))
+soma = 0
+subtracao = 0
+soma_2_primeiros = f1 + f2
+soma_termos = 0
+
+print(f1)
+print(f2)
+
+if numero > 3:
+    for i in range(1, numero-1):
+        if i % 2 == 0 :
+            subtracao = f1 - f2
+
+            print(subtracao)
+
+            f1 = f2
+            f2 = subtracao
+
+            soma_termos = soma_termos + subtracao
+
+        else:
+            soma = f1 + f2
+
+            print(soma)
+
+            f1 = f2
+            f2 = soma
+
+            soma_termos = soma_termos + soma
+
+    soma_termos = soma_termos + soma_2_primeiros
+    print("soma termos", soma_termos)
+else:
+    print("A posição da serie deve ser maior que 3.")
+
