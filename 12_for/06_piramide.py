08320697f125ea122df00ae7cbdf323b8b0d4b63
Bruna Maria <bruna682m@gmail.com>
2026-08-31 18:14:14 -0300

    Criar 06_piramide.py
    exercício pirâmide de números

---
+++ 12_for/06_piramide.py
@@ -0,0 +1,29 @@
+for a in range(1, 1+1):
+    print(a)
+    for b in range(2, 2+1):
+        print(a, b)
+        for c in range (3, 3+1):
+         print(a,b,c)
+         for d in range(4, 4+1):
+             print(a,b,c,d)
+             for e in range(5, 5+1):
+                 print(a,b,c,d,e)
+
+
+# %%
+for a in range(1, 1+1):
+    print(a)
+    for b in range(2, 2+1):
+        print(b, b)
+        for c in range (3, 3+1):
+         print(c,c,c)
+         for d in range(4, 4+1):
+             print(d,d,d,d)
+             for e in range(5, 5+1):
+                 print(e,e,e,e,e)
+        
+
+    
+        
+        
+    
