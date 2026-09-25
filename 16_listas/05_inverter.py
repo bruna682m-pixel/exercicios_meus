f7edaa9d2427e58b6c900480843bc2e66f0e3231
bruna <bruna682m@gmail.com>
2026-09-25 11:24:50 -0300

    chegando a solução certa para inverter a lista e descobrir números repetidos

--- 16_listas/05_inverter.py
+++ 16_listas/05_inverter.py
@@ -7,3 +7,15 @@ for i, eun in enumerate(numeros):
        invertida[len(numeros) -1 -i] = eun
 
 print(invertida)
+
+# %%
+
+numeros = [10, 20, 30, 40, 50]
+invertida = []
+
+for i in range(len(numeros)-1, -1, -1):
+       invertida.append(numeros[i])
+
+print(invertida)
+
+ 
--- 16_listas/06_repetidos.py
+++ 16_listas/06_repetidos.py
@@ -5,7 +5,7 @@ repetidos = []
 
 for i, enu in enumerate(numeros):
     for j, outro_enu in enumerate(numeros):
-        if numeros == outro_enu and i != j:
+        if enu == outro_enu and i != j:
             if enu not in repetidos:
                 repetidos.append(enu)
 
